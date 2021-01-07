13535f326c690944b401ad13e245051cff03d569
turavinin <aturavinin@gmail.com>
2021-01-07 13:46:53 -0300

    Add project files.

---
+++ RockPaperScissorsConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissorsConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
